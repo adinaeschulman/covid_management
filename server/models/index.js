a292036e7464bb03ea9360dd7c30031e8029091f
adinaeschulman <aschulma@g.jct.ac.il>
2023-05-06 22:13:56 +0300

    sync from sequelize with force true optional

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,9 @@
 const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
+
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
-
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
@@ -18,5 +18,15 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.Employee = require("./employee.js")(sequelize, Sequelize);
+
+(async () => {
+  try {
+    // await db.sequelize.sync({force: true}); // notice the force: true and read what it does
+    // await db.sequelize.sync()
+    console.log("All models were synchronized successfully.");
+  } catch (error) {
+    console.error("An error occurred while synchronizing the models:", error);
+  }
+})();
 
 module.exports = db;
